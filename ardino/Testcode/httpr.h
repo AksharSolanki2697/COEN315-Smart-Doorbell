8bfa266ef5f5bcb1a30226f7baa4c2f364ae6e38
unknown <travisle22@gmail.com>
2020-03-12 19:23:20 -0700

    Added getting photos method

--- ardino/Testcode/httpr.h
+++ ardino/Testcode/httpr.h
@@ -1,37 +1,95 @@
-#include <Wifi.h>
-#include <HTTPClient.h>
+#include "WiFi.h"
+#include <FS.h>
+#include <SPIFFS.h>
 
-void sendPostData()
+#define BOUNDARY "---------xx---xx--------" 
+#define TIMEOUT 20000
+const char* host = "data.sparkfun.com";
+String body(String content , String message);
+String header(size_t length);
+
+
+void sendpicture()
 {
-  //got from https://techtutorialsx.com/2017/05/20/esp32-http-post-requests/
-if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
-   HTTPClient http;   
+  File file = SPIFFS.open("/photo.jpg", FILE_READ);
+  if (!file) {
+      Serial.println("Failed to open file in reading mode");
+      return;
+  }
+  String bodyTxt =  body("message","Picture");
+  String bodyPic =  body("imageFile","Picture");
+  String bodyEnd =  String("--")+BOUNDARY+String("--\r\n");
+  size_t allLen = bodyTxt.length()+bodyPic.length()+file.size()+bodyEnd.length();
+  String headerTxt =  header(allLen);
+  const int httpPort = 80;
+  WiFiClient client;
+  if (!client.connect(host, httpPort)) {
+       Serial.println("connection failed");
+       file.close();
+       return;
+  }
+  Serial.print("yay");
 
-   http.begin("http://172.31.154.186:5000/");  //Specify destination for HTTP request
-   http.addHeader("Content-Type", "text/plain");             //Specify content-type header
-   delay(2000);
-   int httpResponseCode;   //Send the actual POST request
-   httpResponseCode= http.POST("q=1");
-   if(httpResponseCode>0){
- 
-    String response = http.getString();                       //Get the response to the request
- 
-    Serial.println(httpResponseCode);   //Print return code
-    Serial.println(response);           //Print request answer
- 
-   }else{
- 
-    Serial.print("Error on sending POST: ");
-    Serial.println(httpResponseCode);
- 
-   }
- 
-   http.end();  //Free resources
- 
- }else{
- 
-    Serial.println("Error in WiFi connection");   
- 
- }
+  client.print(headerTxt+bodyTxt+bodyEnd);
+  while (file.available()){
+    client.write(file.read());
+  }
+  client.print("\r\n"+bodyEnd);
   
+  file.close();
+
+  delay(20);
+  long tOut = millis() + TIMEOUT;
+  while(client.connected() && tOut > millis()) 
+  {
+   if (client.available()) 
+   {
+     String serverRes = client.readStringUntil('\r');
+       Serial.println(serverRes);
+   }
+  }
+  return;
+}
+
+String header(size_t length)
+{
+  String  data;
+      data =  F("POST /ln/bot.php HTTP/1.1\r\n");
+      data += F("cache-control: no-cache\r\n");
+      data += F("Content-Type: multipart/form-data; boundary=");
+      data += BOUNDARY;
+      data += "\r\n";
+      data += F("User-Agent: PostmanRuntime/6.4.1\r\n");
+      data += F("Accept: */*\r\n");
+      data += F("Host: ");
+      data += host;
+      data += F("\r\n");
+      data += F("accept-encoding: gzip, deflate\r\n");
+      data += F("Connection: keep-alive\r\n");
+      data += F("content-length: ");
+      data += String(length);
+      data += "\r\n";
+      data += "\r\n";
+    return(data);
+}
+String body(String content , String message)
+{
+  String data;
+  data = "--";
+  data += BOUNDARY;
+  data += F("\r\n");
+  if(content=="imageFile")
+  {
+    data += F("Content-Disposition: form-data; name=\"imageFile\"; filename=\"picture.jpg\"\r\n");
+    data += F("Content-Type: image/jpeg\r\n");
+    data += F("\r\n");
+  }
+  else
+  {
+    data += "Content-Disposition: form-data; name=\"" + content +"\"\r\n";
+    data += "\r\n";
+    data += message;
+    data += "\r\n";
+  }
+   return(data);
 }
