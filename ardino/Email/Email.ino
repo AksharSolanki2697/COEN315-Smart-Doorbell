0e9bfcc27082f6a0d36d5ad4c2ccfce66c046715
unknown <travisle22@gmail.com>
2020-03-14 19:39:31 -0700

    new email

--- ardino/Email/Email.ino
+++ ardino/Email/Email.ino
@@ -1,19 +1,17 @@
 #include "ESP32_MailClient.h"
-#include <WiFiAP.h>
-#include <WiFiClient.h>"
-#include "ESPAsyncWebServer.h"
+#include <WiFiClient.h>
+#include <HTTPClient.h>
 #include "functions.h"
-//#include "other.h"
-
-AsyncWebServer server(80);
+#include "other.h"
+#include "SPIFFS.h"
 
 //Network Password
 const char* ssid = "SCU-Guest";
 const char* password = "";
 
 //AP newtowrk stuff
-const char *ssidA = "travis";
-const char *passwordA = "";
+const char *ssida = "travis";
+const char *passworda = "";
 
 //timer stuff:
 volatile int interruptCounter;
@@ -25,6 +23,7 @@ portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 void sendCallback(SendStatus info);
 void IRAM_ATTR logouput();
 void startTimer();
+void getPhoto();
 
 void setup(){
   //Setting up pins
@@ -36,12 +35,6 @@ void setup(){
   //set up serial
   Serial.begin(115200);
   Serial.println();
-
-  //setup softAp
-  WiFi.softAP(ssidA, passwordA);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
   
   //setting up wifi
   WiFi.begin(ssid, password);
@@ -51,31 +44,24 @@ void setup(){
   }
   Serial.println("this is the local IP");
   Serial.println(WiFi.localIP());
-
-  //set generate request
-  server.on("/email", HTTP_GET, [](AsyncWebServerRequest *request){
-    generateEmail();
-    request->send(200, "text/plain", "Hello World");
-  });
-  
-  //other method
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    int a=0;
-    request->send(200, "text/plain", "a");
-  });
-  server.begin();
   Serial.println();
-}
 
+  if(!SPIFFS.begin(true)){
+    Serial.println("An error has occured while mounting SPIFFS");
+    return;
+  }
+}
 void loop() {
-  WiFiClient client = server.available();
+  /*
   if (client)
   {
     client.stop();
     Serial.println("Client");
   }
+  */
   if (digitalRead(19) == HIGH)
   {
+    
     ledcWrite(0,300);
     if(interruptCounter <= 0 && iniCount <= 0)
     {
@@ -83,6 +69,18 @@ void loop() {
       interruptCounter++;
       iniCount++;
       portEXIT_CRITICAL(&timerMux);
+      WiFi.disconnect();
+      Serial.println("disconnecting--------------");
+      Serial.print("Connecting to ");
+      Serial.println(ssida);
+    
+      WiFi.begin(ssida, passworda);
+    
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+      }
+      sendRequest();
       startTimer();
     }
   }
@@ -97,6 +95,19 @@ void loop() {
       portEXIT_CRITICAL(&timerMux);
       Serial.println("supposed to send email.");
       stopTimer();
+      getPhoto();
+      WiFi.disconnect();
+      Serial.println("disconnecting--------------");
+      Serial.print("Connecting to ");
+      Serial.println(ssid);
+    
+      WiFi.begin(ssid, password);
+    
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+      }
+      generateEmail();
   }
   delay(100);
 }
@@ -108,7 +119,7 @@ void startTimer()
     timer = timerBegin(0,80,true);
     timerAttachInterrupt(timer, &logouput, true);
   }
-  timerAlarmWrite(timer, 5000000, true);
+  timerAlarmWrite(timer, 12000000, true);
 
   yield();
   timerAlarmEnable(timer);
@@ -132,4 +143,78 @@ void IRAM_ATTR logouput()
   interruptCounter--;
   portEXIT_CRITICAL_ISR(&timerMux);
   
+}
+
+void getPhoto()
+{
+
+  File file = SPIFFS.open("/test-file.jpg",FILE_WRITE);
+  
+  if(!file)
+  {
+    Serial.println("There was an error opening the file for writing");
+    return;
+  }
+
+ if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+ 
+    HTTPClient http;
+    http.begin("http://192.168.4.1/saved-photo"); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+ 
+    if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+
+                // get lenght of document (is -1 when Server sends no Content-Length header)
+                int len = http.getSize();
+
+                // create buffer for read
+                uint8_t buff[128] = { 0 };
+
+                // get tcp stream
+                WiFiClient * stream = http.getStreamPtr();
+
+                // read all data from server
+                while(http.connected() && (len > 0 || len == -1)) {
+                    // get available data size
+                    size_t size = stream->available();
+
+                    if(size) {
+                        // read up to 128 byte
+                        int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+
+                        // write it to Serial
+                        Serial.write(buff, c);
+
+                         if(file.write(buff,c)){
+                            Serial.println("File was written");
+                         }else
+                         { 
+                             Serial.println("File write failed");
+                         }
+                         
+
+                        if(len > 0) {
+                            len -= c;
+                        }
+                    }
+                    delay(1);
+                }
+
+                Serial.println();
+                Serial.print("[HTTP] connection closed or file end.\n");
+
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+    file.close();
+
 }
--- ardino/Email/other.h
+++ ardino/Email/other.h
@@ -1,27 +1,27 @@
 #include <HTTPClient.h>
 
-uint8_t[] sendRequest()
+String sendRequest()
 {
   String response;
   if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
  
    HTTPClient http;   
  
-   http.begin("http://192.168.4.2");  //Specify destination for HTTP request
+   http.begin("http://192.168.4.1/capture");  //Specify destination for HTTP request
    http.addHeader("Content-Type", "text/plain");             //Specify content-type header
  
    int httpResponseCode = http.GET();   //Send the actual POST request
  
    if(httpResponseCode>0){
  
-    uint8_t[] = http.getString();                       //Get the response to the request
+    String repsonse = http.getString();                       //Get the response to the request
  
     Serial.println(httpResponseCode);   //Print return code
     Serial.println(response);           //Print request answer
  
    }else{
  
-    Serial.print("Error on sending POST: ");
+    Serial.print("Error on sending Get: ");
     Serial.println(httpResponseCode);
    }
  
